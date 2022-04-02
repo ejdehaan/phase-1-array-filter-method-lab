a6e1d34088f500c8939efbd9aa72da11009a8766
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-04-01 20:11:07 -0400

    Couldn't pass all tests

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(array, element) {
+    if (element == array.element)
+        return array.element
+    else (element != element)
+        return []
+}
+
+function filter(collection,cb) {
+    for (const element of collection) {
+        if (findMatching()) {
+            console.log(element)
+        }
+    }
+}
+
